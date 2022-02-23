80537cb3ab0ee0543ea1b96c5064393c1727ef40
bakary <bakarykone729@gmail.com>
2022-02-23 22:54:54 +0100

    7th commit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,4 +1,4 @@
-import data from '../data/contact.json'
+import data from "../data/contact.json";
 
 function Contact() {
   return (
@@ -8,8 +8,11 @@ function Contact() {
         <p className="contactsubt">{data.contacts.desc}</p>
       </div>
       <div className="contactFormSubmit">
-        <input type="email" placeholder={data.contacts.input}></input>
-        <button className="btn2">{data.contacts.btn}</button>
+        <form name="contactForm" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contactForm"/>
+          <input type="email" name="email" placeholder={data.contacts.input} />
+          <button type="submit" className="btn2">{data.contacts.btn}</button>
+        </form>
       </div>
     </div>
   );
